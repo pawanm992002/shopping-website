6782d366f36b994db4a2fe044b7a8e434bb6ae06
pawan <pawanm992002@gmail.com>
2022-02-13 22:34:59 +0530

    first website

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs')
 require('./db/conn')
 const customerDetail = require('./models/customer')
 const bcrypt = require('bcrypt')
+const res = require('express/lib/response')
 
 
 const port = process.env.PORT || 8000
@@ -25,13 +26,51 @@ hbs.registerPartials(partialsPath)
 app.get("/",(req,res)=>{
     res.render('index')
 })
+
+app.get("/mobiles",(req,res)=>{
+    res.render('mobiles')
+})
+app.get("/electronics",(req,res)=>{
+    res.render('electronics')
+})
+app.get("/fashion",(req,res)=>{
+    res.render('fashion')
+})
+app.get("/home",(req,res)=>{
+    res.render('home')
+})
+app.get("/grocery",(req,res)=>{
+    res.render('grocery')
+})
+app.get("/beauty",(req,res)=>{
+    res.render('beauty')
+})
+app.get("/toys",(req,res)=>{
+    res.render('toys')
+})
+app.get("/study",(req,res)=>{
+    res.render('study')
+})
+app.get("/kichen",(req,res)=>{
+    res.render('kichen')
+})
+app.get("/sports",(req,res)=>{
+    res.render('sports')
+})
 app.get("/register",(req,res)=>{
     res.render('register')
+})
+app.get("/about",(req,res)=>{
+    res.render('about')
+})
+app.get("/services",(req,res)=>{
+    res.render('services')
 })
 
 app.get("/login",(req,res)=>{
     res.render('login')
 })
+
 app.post("/register", async (req,res)=>{
     try {
         const password = req.body.password
@@ -67,6 +106,10 @@ app.post("/login",async (req,res)=>{
         res.status(404)
         res.send("email or password is wrong")
     }
+})
+
+app.get('*',(req,res)=>{
+    res.status(404).render("404")
 })
 
 app.listen(port,()=>{
